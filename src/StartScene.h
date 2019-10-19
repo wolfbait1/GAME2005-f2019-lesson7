e4810c0ff62d9ea8e84233b490e526f1e0b4ce51
Alexander Pacheco <gamerkid9666@gmail.com>
2019-10-18 23:06:36 -0400

    configured imgui for start scene

--- src/StartScene.h
+++ src/StartScene.h
@@ -24,6 +24,18 @@ private:
 	Label* m_pInstructionsLabel;*/
 
 	Ship* m_pShip;
+
+	glm::vec2 m_mousePosition;
+
+	// ImGui utility functions
+	void m_ImGuiKeyMap();
+	void m_ImGuiSetStyle();
+	void m_updateUI();
+
+	// ImGui menu variables
+	bool m_exitApp = false;
+	bool m_displayAbout = false;
+	bool m_displayUI = true;
 };
 
 #endif /* defined (__START_SCENE__) */
